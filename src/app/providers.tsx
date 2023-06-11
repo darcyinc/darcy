272a004ec3269f726b717bb6b944f202fac673eb
Davi Patricio <davipatricio2006@gmail.com>
2023-06-11 21:32:37 +0000

    add theme hook and save user theme

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,17 +1,28 @@
 'use client';
 
+import { useEffect, useState } from 'react';
 import { ThemeProvider } from 'styled-components';
 
 import MobileBottomNavbar from '@/components/LeftNavbar/MobileBottomNavbar';
 import ShowIfMediaQuery from '@/components/ShowIfMediaQuery';
+import { useTheme } from '@/hooks/useTheme';
 import StyledComponentsRegistry from '@/lib/registry';
 import GlobalStyles from '@/styles/GlobalStyles';
 import { darkTheme } from '@/styles/themes/dark';
 import { lightTheme } from '@/styles/themes/light';
 import { slateTheme } from '@/styles/themes/slate';
 
+const themes = {
+  dark: darkTheme,
+  light: lightTheme,
+  slate: slateTheme,
+};
+
 export default function Providers({ children }: { children: React.ReactNode }) {
-  const theme = darkTheme || lightTheme || slateTheme;
+  const [theme, setTheme] = useState(darkTheme);
+  const { theme: userTheme } = useTheme();
+
+  useEffect(() => setTheme(themes[userTheme]), [userTheme]);
 
   return (
     <ThemeProvider theme={theme}>
---
+++ src/hooks/useTheme.ts
@@ -0,0 +1,20 @@
+import { create } from 'zustand';
+import { persist, createJSONStorage } from 'zustand/middleware';
+
+interface ITheme {
+  theme: 'dark' | 'light' | 'slate';
+  setTheme: (theme: ITheme['theme']) => void;
+}
+
+export const useTheme = create(
+  persist<ITheme>(
+    (set) => ({
+      theme: 'dark',
+      setTheme: (theme) => set({ theme }),
+    }),
+    {
+      name: 'theme',
+      storage: createJSONStorage(() => localStorage),
+    }
+  )
+);
