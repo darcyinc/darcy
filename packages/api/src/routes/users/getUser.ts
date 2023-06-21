61d1e51d65cff9ad2b87e6e0f6b41120b456a433
Davi Patricio <davipatricio2006@gmail.com>
2023-06-21 23:03:42 +0000

    initial getUser route

---
+++ packages/api/src/routes/users/getUser.ts
@@ -0,0 +1,37 @@
+import { FastifyInstance, RouteOptions } from 'fastify';
+
+export default async function (
+  fastify: FastifyInstance,
+  _options: RouteOptions
+) {
+  fastify.route<{
+    Params: {
+      handle: string;
+    };
+  }>({
+    method: 'GET',
+    url: '/:handle',
+    config: {
+      requiresAuth: false,
+    },
+    handler: async (request, reply) => {
+      const user = await prisma.user.findFirst({
+        where: {
+          handle: request.params.handle,
+        },
+        include: {
+          auth: false,
+          posts: false,
+        },
+      });
+
+      if (!user) {
+        return reply.status(404).send({
+          error: 'User not found',
+        });
+      }
+
+      return user;
+    },
+  });
+}
