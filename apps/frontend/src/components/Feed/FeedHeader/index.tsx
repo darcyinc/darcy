afdadb70a120f7f259e4e30466e659e162e30c33
Davi Patricio <davipatricio2006@gmail.com>
2023-07-09 20:00:56 +0000

    port FeedHeader

--- apps/frontend/src/components/Feed/FeedHeader/index.tsx
+++ apps/frontend/src/components/Feed/FeedHeader/index.tsx
@@ -1,8 +1,7 @@
 'use client';
 
+import clsx from 'clsx';
 import { useCallback } from 'react';
-
-import { Container } from './styles';
 
 type FilterOption = 'foryou' | 'newest';
 
@@ -19,17 +18,25 @@ export default function FeedHeader({ filter, i18nTitle, i18nForYou, i18nFollowin
   }, []);
 
   return (
-    <Container>
-      <h3>{i18nTitle}</h3>
+    <header className="w-full border-b border-grayBorder">
+      <h3 className="select-none px-5 py-4">{i18nTitle}</h3>
 
       {['foryou', 'newest'].map((item) => (
-        <button key={item} type="button" onClick={() => handleFilter(item as FilterOption)}>
-          <div>
-            <span>{item === 'foryou' ? i18nForYou : i18nFollowing}</span>
-            {filter === item && <div className="divider" />}
+        <button
+          className={clsx(
+            'h-10 w-2/4 cursor-pointer border-none bg-none text-base hover:bg-hoverEffect',
+            filter === item ? 'text-primary font-bold' : 'h-11'
+          )}
+          key={item}
+          type="button"
+          onClick={() => handleFilter(item as FilterOption)}
+        >
+          <div className="text-secondary m-auto w-fit">
+            <span className="leading-10">{item === 'foryou' ? i18nForYou : i18nFollowing}</span>
+            {filter === item && <div className="h-1 rounded-sm bg-blue" />}
           </div>
         </button>
       ))}
-    </Container>
+    </header>
   );
 }
