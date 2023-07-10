2d1a9f88cc293ace33456238caecd5a8543f1d15
Davi Patricio <davipatricio2006@gmail.com>
2023-07-10 21:02:06 +0000

    simplify divider

--- apps/frontend/src/components/Divider/index.tsx
+++ apps/frontend/src/components/Divider/index.tsx
@@ -6,7 +6,7 @@ export default function Divider({ text }: DividerProps) {
   const addMargin = Boolean(text);
 
   return (
-    <span className="flex w-full select-none items-center px-0.5 text-center">
+    <span className="flex w-full select-none items-center px-0.5">
       <div className="h-px flex-grow bg-grayBorder content-['']" />
       {addMargin && <span className="mx-2">{text}</span>}
       <div className="h-px flex-grow bg-grayBorder content-['']" />
