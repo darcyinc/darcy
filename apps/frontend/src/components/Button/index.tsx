1eb0b8b87be75393c87c220f7f2459865f2ac040
Davi Patricio <davipatricio2006@gmail.com>
2023-07-10 10:53:08 +0000

    make large button a bit smaller

--- apps/frontend/src/components/Button/index.tsx
+++ apps/frontend/src/components/Button/index.tsx
@@ -12,7 +12,7 @@ export default function Button({ variant, children, size, ...props }: ButtonProp
         'flex cursor-pointer appearance-none  items-center justify-center rounded-full border-none text-lg font-bold capitalize hover:opacity-80 disabled:cursor-not-allowed disabled:opacity-70',
         variant === 'blue' && 'bg-blue text-[#fff]',
         variant === 'white' && 'bg-white text-blue',
-        size === 'small' ? 'p-2' : 'p-4',
+        size === 'small' ? 'p-2' : 'p-2.5',
         props.className
       )}
       type="button"
