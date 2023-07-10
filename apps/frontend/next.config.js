8f736ef4be4def450ff5d23a6cc608d2bef252a7
Davi Patricio <davipatricio2006@gmail.com>
2023-07-10 15:40:47 +0000

    disable server actions

--- apps/frontend/next.config.js
+++ apps/frontend/next.config.js
@@ -2,9 +2,6 @@ const withNextIntl = require('next-intl/plugin')('./src/lib/i18n.ts');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    serverActions: true,
-  },
   reactStrictMode: false,
 };
 
