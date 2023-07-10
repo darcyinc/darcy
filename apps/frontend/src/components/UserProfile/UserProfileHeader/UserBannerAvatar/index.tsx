7e18bf7efb4df00dbea34afa4f3f53ca3704394a
Davi Patricio <davipatricio2006@gmail.com>
2023-07-10 16:45:58 +0000

    improve avatar ux

--- apps/frontend/src/components/UserProfile/UserProfileHeader/UserBannerAvatar/index.tsx
+++ apps/frontend/src/components/UserProfile/UserProfileHeader/UserBannerAvatar/index.tsx
@@ -25,16 +25,13 @@ export default function UserBannerAvatar({ avatar, banner }: UserBannerAvatar) {
 
       <img alt="User banner" className="max-h-52 w-full object-cover" decoding="async" draggable={false} loading="lazy" src={banner} />
 
-      <button type="button" onClick={() => setAvatarModalOpen(true)}>
+      <button
+        className="absolute -bottom-[25%] ml-5 h-28 w-28 overflow-hidden rounded-full border-2 border-background transition-all duration-700 hover:border-gray-500 sm:-bottom-[50px] sm:h-32 sm:w-32"
+        type="button"
+        onClick={() => setAvatarModalOpen(true)}
+      >
         {/* eslint-disable-next-line jsx-a11y/img-redundant-alt */}
-        <img
-          alt="User profile picture"
-          className="absolute -bottom-[15%] ml-5 h-28 w-28 rounded-full border border-background object-cover sm:-bottom-[50px] sm:h-32 sm:w-32"
-          decoding="async"
-          draggable={false}
-          loading="lazy"
-          src={avatar}
-        />
+        <img alt="User profile picture" className="object-cover" decoding="async" draggable={false} loading="lazy" src={avatar} />
       </button>
     </div>
   );
