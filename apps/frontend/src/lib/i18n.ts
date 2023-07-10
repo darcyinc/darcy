7e93ca1ceef7b55e0cb03ac176c87344a45fef49
Davi Patricio <davipatricio2006@gmail.com>
2023-07-10 20:32:50 +0000

    make i18n more readable

--- apps/frontend/src/lib/i18n.ts
+++ apps/frontend/src/lib/i18n.ts
@@ -1,7 +1,8 @@
-/* eslint-disable @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access, unicorn/no-await-expression-member, @typescript-eslint/restrict-template-expressions */
+import { AbstractIntlMessages } from 'next-intl';
 import { getRequestConfig } from 'next-intl/server';
 
-// eslint-disable-next-line import/no-unused-modules, @typescript-eslint/no-unsafe-call
-export default getRequestConfig(async ({ locale }) => ({
-  messages: (await import(`../locales/${locale}.json`)).default
-}));
+// eslint-disable-next-line import/no-unused-modules
+export default getRequestConfig(async ({ locale }) => {
+  const messages = (await import(`../locales/${locale}.json`)) as { default: AbstractIntlMessages };
+  return { messages };
+});
