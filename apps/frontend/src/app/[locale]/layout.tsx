c0929dec15f87322a7ccb8f27a395c4b4a02b9e2
Davi Patricio <davipatricio@pm.me>
2023-07-21 11:30:30 -0300

    fix missing root layout

--- apps/frontend/src/app/[locale]/layout.tsx
+++ apps/frontend/src/app/[locale]/layout.tsx
@@ -1,27 +1,10 @@
-import { Metadata } from 'next';
-import dynamic from 'next/dynamic';
-import { Inter } from 'next/font/google';
 import { notFound } from 'next/navigation';
 import { PropsWithChildren } from 'react';
-import { DESCRIPTION } from '@/util/constants';
 import { useLocale } from 'next-intl';
-
-import '@/styles/tailwind.css';
-import '@/styles/global.scss';
 
 interface LocaleRouteParams {
   locale: string;
 }
-
-const Providers = dynamic(() => import('./providers'));
-
-const inter = Inter({ subsets: ['latin'], display: 'swap' });
-
-export const metadata: Metadata = {
-  title: 'Your Feed | Darcy',
-  description: DESCRIPTION,
-  keywords: ['social network', 'open source']
-};
 
 export default function RootLayout({ children, params }: PropsWithChildren<{ params: LocaleRouteParams }>) {
   const locale = useLocale();
@@ -31,11 +14,5 @@ export default function RootLayout({ children, params }: PropsWithChildren<{ par
     notFound();
   }
 
-  return (
-    <html lang="en">
-      <body className={inter.className}>
-        <Providers>{children}</Providers>
-      </body>
-    </html>
-  );
+  return children;
 }
