57fea01f62c75b1312e958b5f83d33e7d7372ef5
Davi Patricio <davipatricio@pm.me>
2023-08-09 20:43:02 -0300

    add initial user page

---
+++ apps/darcy/src/app/[locale]/(social)/[username]/page.tsx
@@ -0,0 +1,22 @@
+import Link from 'next/link';
+import { MdArrowBack } from 'react-icons/md';
+
+import Feed from '@/components/Feed';
+import FeedHeader from '@/components/Feed/FeedHeader';
+
+export default function Home() {
+  return (
+    <Feed>
+      <FeedHeader className="flex items-center gap-4 p-4">
+        <Link className="text-textPrimary hover:text-textSecondary" href="/">
+          <MdArrowBack className="h-7 w-7" />
+        </Link>
+
+        <div>
+          <h1 className="text-xl font-bold text-textPrimary">Davi Patricio</h1>
+          <p className="text-sm text-textSecondary">@davipatricio</p>
+        </div>
+      </FeedHeader>
+    </Feed>
+  );
+}
