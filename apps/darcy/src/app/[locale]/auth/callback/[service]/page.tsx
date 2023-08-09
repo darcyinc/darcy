e1ab4591eb12d823969d3ed19a69ac20e677e63f
Davi Patricio <davipatricio@pm.me>
2023-08-09 20:17:15 -0300

    add initial auth callback page

---
+++ apps/darcy/src/app/[locale]/auth/callback/[service]/page.tsx
@@ -0,0 +1,75 @@
+'use client';
+
+import Link from 'next/link';
+import { useRouter } from 'next/navigation';
+import { useTranslations } from 'next-intl';
+import { useEffect, useState } from 'react';
+
+import { AUTH_SERVICES_CALLBACK } from '@/utils/constants';
+
+interface CallbackPageParams {
+  service: string;
+}
+
+interface CallbackPageSearchParams {
+  code: string;
+  state: string;
+}
+
+interface CallbackPageProps {
+  params: CallbackPageParams;
+  searchParams: CallbackPageSearchParams;
+}
+
+export default function CallbackPage({ params, searchParams }: CallbackPageProps) {
+  const t = useTranslations('AuthCallback');
+
+  const { service } = params;
+  const { code, state } = searchParams;
+
+  const router = useRouter();
+  const [error, setError] = useState('');
+
+  if (error) {
+    return (
+      <span className="m-auto text-center text-xl">
+        <p>{error}</p>
+        <Link href="/auth/signin">{t('goBack')}</Link>
+      </span>
+    );
+  }
+
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  useEffect(() => {
+    const auth = async () => {
+      const oauth2State = sessionStorage.getItem(`oauth2-state:${service}`);
+
+      if (!AUTH_SERVICES_CALLBACK.includes(service) || !code || !state) return router.replace('/auth');
+
+      if (state !== oauth2State) return router.replace('/auth/signin');
+
+      // const { error, redirect, token } = await client.auth.withService({
+      //   code,
+      //   service
+      // });
+
+      const token = '',
+        error = '',
+        redirect = false;
+
+      if (redirect) return router.replace('/auth/signin');
+      if (error) return setError(error);
+
+      if (token) {
+        sessionStorage.removeItem(`oauth2-state:${service}`);
+        sessionStorage.setItem('token', token);
+
+        router.push('/');
+      }
+    };
+
+    auth();
+  }, [code, router, service, state]);
+
+  return <span className="m-auto text-xl">{t('authenticating')}</span>;
+}
