7671c8f3920de0b5d454b9af8e97c04f0bbdccf3
Davi Patricio <davipatricio@pm.me>
2023-08-08 22:02:19 -0300

    improve button component

--- apps/darcy/src/components/Button/index.tsx
+++ apps/darcy/src/components/Button/index.tsx
@@ -1,6 +1,6 @@
 import clsx from 'clsx';
 
-interface ButtonProps extends React.HTMLAttributes<HTMLButtonElement> {
+interface ButtonProps extends React.ComponentProps<'button'> {
   color: 'blue' | 'white';
   size: 'sm' | 'md' | 'lg';
 }
@@ -10,9 +10,9 @@ export default function Button({ children, color = 'blue', size = 'md', ...props
     <button
       {...props}
       className={clsx(
-        'flex items-center justify-center rounded-full p-2 text-xl font-bold',
-        color === 'blue' && 'bg-blue text-white hover:bg-blue/80',
-        color === 'white' && 'bg-white text-black hover:bg-gray-300',
+        'flex items-center justify-center rounded-full px-5 py-2 font-bold disabled:cursor-not-allowed',
+        color === 'blue' && 'bg-blue text-white hover:bg-blue/80 disabled:bg-blue/50 disabled:text-white/50',
+        color === 'white' && 'bg-white text-black hover:bg-gray-300 disabled:bg-gray-300 disabled:text-black/70',
         size === 'sm' && 'text-sm',
         size === 'md' && 'text-base',
         size === 'lg' && 'w-full p-5 text-xl',
