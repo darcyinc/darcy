080698927aafffa3781c1859f640e54f552a5d63
Davi Patricio <davipatricio@pm.me>
2023-08-09 20:39:30 -0300

    make main use up to 600px

--- apps/darcy/src/app/[locale]/(social)/layout.tsx
+++ apps/darcy/src/app/[locale]/(social)/layout.tsx
@@ -10,7 +10,7 @@ export default function RootLayout({ children }: PropsWithChildren) {
       <MobileNavbar />
       <AsideNavbar />
 
-      <main>{children}</main>
+      <main className="w-full max-w-[600px]">{children}</main>
 
       <Trending />
     </>
