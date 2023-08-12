f2a4bb908a2c2ad648146ca9cbb48be320751eb5
Davi Patricio <davipatricio@pm.me>
2023-08-12 15:27:54 -0300

    improve exception handling

--- packages/api/apps/root/src/exceptions/rpcExpection.filter.ts
+++ packages/api/apps/root/src/exceptions/rpcExpection.filter.ts
@@ -5,10 +5,10 @@ import { FastifyReply } from 'fastify';
 @Catch(RpcException)
 export class RpcExceptionFilter implements ExceptionFilter {
   catch(exception: RpcException, host: ArgumentsHost) {
-    const error: any = exception.getError();
+    const error = exception.getError() as Record<string, any> | undefined;
     const ctx = host.switchToHttp();
     const response = ctx.getResponse<FastifyReply>();
 
-    response.status(error.statusCode).send(error);
+    response.status(error?.statusCode ?? 500).send(error ?? 'Internal server error');
   }
 }
