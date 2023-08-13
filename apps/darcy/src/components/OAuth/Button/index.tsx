7ce0ffcfa793812f52bad0976b8829997c6f9fef
Davi Patricio <davipatricio@pm.me>
2023-08-13 14:43:22 -0300

    add provider icons

--- apps/darcy/src/components/OAuth/Button/index.tsx
+++ apps/darcy/src/components/OAuth/Button/index.tsx
@@ -2,6 +2,9 @@
 
 import { PropsWithChildren, useCallback } from 'react';
 
+import { BsDiscord, BsGithub } from 'react-icons/bs';
+import { FcGoogle } from 'react-icons/fc';
+
 import Button from '@/components/Button';
 import clsx from 'clsx';
 
@@ -9,25 +12,33 @@ interface OAuth2ButtonProps {
   service: 'discord' | 'google' | 'github';
 }
 
-const OAuthLinks: Record<OAuth2ButtonProps['service'], string> = {
-  discord: process.env.NEXT_PUBLIC_DISCORD_AUTH_URL!,
-  google: process.env.NEXT_PUBLIC_GOOGLE_AUTH_URL!,
-  github: process.env.NEXT_PUBLIC_GITHUB_AUTH_URL!
-};
-
-const ButtonStyles: Record<OAuth2ButtonProps['service'], string> = {
-  discord: 'bg-indigo-700 !text-white hover:bg-indigo-700/80',
-  google: 'bg-white !text-black hover:bg-white/80',
-  github: 'bg-neutral-600 !text-white hover:bg-neutral-600/80'
+const Services = {
+  discord: {
+    link: process.env.NEXT_PUBLIC_DISCORD_AUTH_URL!,
+    styles: 'bg-indigo-700 !text-white hover:bg-indigo-700/80',
+    Icon: BsDiscord
+  },
+  google: {
+    link: process.env.NEXT_PUBLIC_GOOGLE_AUTH_URL!,
+    styles: 'bg-white !text-black hover:bg-white/80',
+    Icon: FcGoogle
+  },
+  github: {
+    link: process.env.NEXT_PUBLIC_GITHUB_AUTH_URL!,
+    styles: 'bg-neutral-600 !text-white hover:bg-neutral-600/80',
+    Icon: BsGithub
+  }
 }
 
 export default function OAuth2Button({ service, children }: PropsWithChildren<OAuth2ButtonProps>) {
+  const serviceData = Services[service];
+
   const handleRedirect = useCallback(
     (e: React.MouseEvent<HTMLButtonElement>) => {
       e.preventDefault();
 
       const randomString = Math.random().toString(36);
-      const oauthLink = `${OAuthLinks[service]}&state=${randomString}`;
+      const oauthLink = `${serviceData.link}&state=${randomString}`;
 
       sessionStorage.setItem(`oauth2-state:${service}`, randomString);
 
@@ -37,7 +48,8 @@ export default function OAuth2Button({ service, children }: PropsWithChildren<OA
   );
 
   return (
-    <Button className={ButtonStyles[service]} color="blue" size="md" type="button" onClick={handleRedirect}>
+    <Button className={clsx('gap-2', serviceData.styles)} color="blue" size="md" type="button" onClick={handleRedirect}>
+      <serviceData.Icon className="w-6 h-6" />
       {children}
     </Button>
   );
