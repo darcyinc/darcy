19424403d5059da00d186ee21b190e1a5811fa74
Davi Patricio <davipatricio@pm.me>
2023-08-14 22:15:39 -0300

    clean up button component

--- apps/darcy/src/components/Button/index.tsx
+++ apps/darcy/src/components/Button/index.tsx
@@ -5,17 +5,25 @@ interface ButtonProps extends React.ComponentProps<'button'> {
   size: 'sm' | 'md' | 'lg';
 }
 
+const ButtonStyles: Record<ButtonProps['color'], string> = {
+  blue: 'bg-blue text-white hover:bg-blue/80 disabled:bg-blue/50 disabled:text-white/50',
+  white: 'bg-white text-black hover:bg-gray-300 disabled:bg-gray-300 disabled:text-black/70',
+};
+
+const ButtonSizes: Record<ButtonProps['size'], string> = {
+  sm: 'text-sm',
+  md: 'text-base',
+  lg: 'w-full p-5 text-xl',
+};
+
 export default function Button({ children, color = 'blue', size = 'md', ...props }: ButtonProps) {
   return (
     <button
       {...props}
       className={clsx(
         'flex items-center justify-center rounded-full px-5 py-2 font-bold disabled:cursor-not-allowed',
-        color === 'blue' && 'bg-blue text-white hover:bg-blue/80 disabled:bg-blue/50 disabled:text-white/50',
-        color === 'white' && 'bg-white text-black hover:bg-gray-300 disabled:bg-gray-300 disabled:text-black/70',
-        size === 'sm' && 'text-sm',
-        size === 'md' && 'text-base',
-        size === 'lg' && 'w-full p-5 text-xl',
+        ButtonStyles[color],
+        ButtonSizes[size],
         props.className
       )}
       type="button"
