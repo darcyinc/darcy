53d56168e7f95be68a318b83252edfb7f9e18a7d
Davi Patricio <davipatricio@pm.me>
2023-08-19 19:51:30 -0300

    remove root route

--- packages/api/src/routes/root.ts
+++
@@ -1,9 +0,0 @@
-import { FastifyInstance } from 'fastify';
-
-export interface RouteOptions {
-  prefix: string | undefined;
-}
-
-export default async function (fastify: FastifyInstance, _options: RouteOptions) {
-  fastify.get('/', () => 'Hello, world!');
-}
