9e2574f3a2a978257d21f66fd857d207c1ab6278
Davi Patricio <davipatricio@pm.me>
2023-08-20 23:22:33 -0300

    add requireAuth middleware

--- packages/api/apps/main/middlewares/onError.ts
+++
@@ -1,9 +0,0 @@
-import { InternalServerError } from 'http-errors';
-
-import { DarcyFastifyInstance } from '..';
-
-export default async function (app: DarcyFastifyInstance) {
-  app.setErrorHandler((_error, _request, reply) => {
-    reply.send(InternalServerError());
-  });
-}
---
+++ packages/api/apps/main/middlewares/requireAuth.ts
@@ -0,0 +1,15 @@
+import { Unauthorized } from 'http-errors';
+
+import { DarcyFastifyInstance } from '..';
+import { DarcyRouteConfig } from '../../shared/types/routeConfig';
+
+export default async function (app: DarcyFastifyInstance) {
+  app.addHook('preHandler', async (request, reply) => {
+    const config = request.routeConfig as DarcyRouteConfig;
+
+    if (config.requiresAuth) {
+      if (!request.headers.authorization) return reply.send(Unauthorized('Missing Authentication Header'));
+      if (!request.headers.authorization.startsWith('Bearer ')) return reply.send(Unauthorized('Invalid Authentication Header'));
+    }
+  });
+}
