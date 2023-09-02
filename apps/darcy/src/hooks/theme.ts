effcc0daf57ab5c8f49abaef28651889fb285034
Davi Patricio <davipatricio@pm.me>
2023-09-02 20:51:59 -0300

    remove slate theme

--- apps/darcy/src/hooks/theme.ts
+++ apps/darcy/src/hooks/theme.ts
@@ -2,7 +2,7 @@ import { create } from 'zustand';
 import { persist, createJSONStorage } from 'zustand/middleware';
 
 interface ITheme {
-  theme: 'dark' | 'light' | 'slate';
+  theme: 'dark' | 'light';
   setTheme: (theme: ITheme['theme']) => void;
 }
 
