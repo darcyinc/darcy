d838d74ba2e4bcf5c40fdf369f68fb088c630f7a
Davi Patricio <davipatricio@pm.me>
2023-10-06 08:56:04 -0300

    fix: improve token check middleware

--- packages/api/apps/main/middlewares/requireAuth.ts
+++ packages/api/apps/main/middlewares/requireAuth.ts
@@ -1,5 +1,6 @@
 import { Unauthorized } from 'http-errors';
 
+import { verifyToken } from '@/shared/lib/jwt';
 import { DarcyRouteConfig } from '@/shared/types/routeConfig';
 
 import { DarcyFastifyInstance } from '..';
@@ -9,8 +10,17 @@ export default async function (app: DarcyFastifyInstance) {
     const config = request.routeOptions.config as DarcyRouteConfig;
 
     if (config.requiresAuth) {
-      if (!request.headers.authorization) return reply.send(Unauthorized('Missing Authentication Header'));
-      if (!request.headers.authorization.startsWith('Bearer ')) return reply.send(Unauthorized('Invalid Authentication Header'));
+      const [type, token] = request.headers.authorization?.split('Bearer ') ?? [];
+
+      if (!type) return reply.send(Unauthorized('Missing Authentication Header'));
+      if (type !== 'Bearer') return reply.send(Unauthorized('Invalid Authentication Header'));
+
+      try {
+        const decodedToken = await verifyToken(token);
+        if (!decodedToken.email || !decodedToken.updatedAt) return reply.send(Unauthorized('Invalid Token'));
+      } catch {
+        return reply.send(Unauthorized('Invalid Token'));
+      }
     }
   });
 }
