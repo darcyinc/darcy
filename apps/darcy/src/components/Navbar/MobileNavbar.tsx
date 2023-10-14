8d4265a307ebd6d898572d458e8d2a3627b4790e
Davi Patricio <davipatricio@pm.me>
2023-10-14 01:31:24 -0300

    lint

--- apps/darcy/src/components/Navbar/MobileNavbar.tsx
+++ apps/darcy/src/components/Navbar/MobileNavbar.tsx
@@ -2,7 +2,7 @@ import { NavbarLinks } from './NavbarLinks';
 
 export default function MobileNavbar() {
   return (
-    <div className="sticky bottom-0 w-full border-t border-t-grayBorder p-1 sm:hidden bg-black/80  backdrop-blur-md">
+    <div className="sticky bottom-0 w-full border-t border-t-grayBorder bg-black/80 p-1 backdrop-blur-md sm:hidden">
       <nav className="flex items-center justify-evenly xl:items-start">
         <NavbarLinks.Mobile />
       </nav>
