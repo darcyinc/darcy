af0baf6aacc3b94614b33ffd4239f3950a322ec5
Davi Patricio <davipatricio2006@gmail.com>
2023-12-23 20:10:56 -0300

    fix icon size

--- apps/darcy/src/components/Feed/FeedPost/FeedPostActions/index.tsx
+++ apps/darcy/src/components/Feed/FeedPost/FeedPostActions/index.tsx
@@ -1,13 +1,12 @@
-'use client';
+"use client";
 
+import { BiRepost } from "react-icons/bi";
 import {
   MdFavorite,
   MdOutlineFavoriteBorder,
-  MdOutlineRestartAlt,
   MdOutlineSpeakerNotes,
-  MdRestartAlt,
   MdViewKanban
-} from 'react-icons/md';
+} from "react-icons/md";
 
 interface FeedPostActionsProps {
   comments: number;
@@ -19,23 +18,33 @@ interface FeedPostActionsProps {
   hasReposted: boolean;
 }
 
-export default function FeedPostActions({ comments, reposts, likes, views, postId, hasLiked, hasReposted }: FeedPostActionsProps) {
-  const RepostIcon = hasReposted ? MdRestartAlt : MdOutlineRestartAlt;
+export default function FeedPostActions({
+  comments,
+  reposts,
+  likes,
+  views,
+  postId,
+  hasLiked,
+  hasReposted
+}: FeedPostActionsProps) {
   const LikedIcon = hasLiked ? MdFavorite : MdOutlineFavoriteBorder;
 
   const handleLike = async () => {
-    console.log('Liking post', postId);
+    console.log("Liking post", postId);
   };
 
   const handleRepost = async () => {
-    console.log('Reposting post', postId);
+    console.log("Reposting post", postId);
   };
 
   return (
     <footer className="mt-2 flex justify-evenly text-sm text-gray-500">
-      <button className="group flex select-none items-center gap-1 hover:text-blue" type="button">
+      <button
+        className="group flex select-none items-center gap-1 hover:text-blue"
+        type="button"
+      >
         <div className="rounded-full p-2 group-hover:bg-blue/20">
-          <MdOutlineSpeakerNotes className="h-4 w-4" />
+          <MdOutlineSpeakerNotes className="h-5 w-5" />
         </div>
         <span>{comments}</span>
       </button>
@@ -47,7 +56,7 @@ export default function FeedPostActions({ comments, reposts, likes, views, postI
         onClick={handleRepost}
       >
         <div className="rounded-full p-2 group-hover:bg-green/20">
-          <RepostIcon className="h-4 w-4" />
+          <BiRepost className="h-5 w-5" />
         </div>
         <span>{reposts}</span>
       </button>
@@ -59,12 +68,15 @@ export default function FeedPostActions({ comments, reposts, likes, views, postI
         onClick={handleLike}
       >
         <div className="rounded-full p-2 group-hover:bg-red/20">
-          <LikedIcon className="h-4 w-4" />
+          <LikedIcon className="h-5 w-5" />
         </div>
         <span>{likes}</span>
       </button>
 
-      <button className="group flex select-none items-center gap-1 hover:text-purple-300" type="button">
+      <button
+        className="group flex select-none items-center gap-1 hover:text-purple-300"
+        type="button"
+      >
         <div className="rounded-full p-2 group-hover:bg-purple-500/20">
           <MdViewKanban className="h-4 w-4" />
         </div>
