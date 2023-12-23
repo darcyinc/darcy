944809f678b565537b3b66630cf1fb1792630b34
Davi Patricio <davipatricio2006@gmail.com>
2023-12-23 20:08:33 -0300

    animate FeedHeader and navbar on mobile

--- apps/darcy/src/components/Feed/FeedHeader/index.tsx
+++ apps/darcy/src/components/Feed/FeedHeader/index.tsx
@@ -1,9 +1,55 @@
-import clsx from 'clsx';
-import { ComponentProps } from 'react';
+"use client";
 
-export default function FeedHeader({ children, ...props }: ComponentProps<'div'>) {
+import clsx from "clsx";
+import { ComponentProps, useEffect, useRef, useState } from "react";
+
+export default function FeedHeader({
+  children,
+  ...props
+}: ComponentProps<"div">) {
+  const header = useRef<HTMLDivElement>(null);
+  const [lastScroll, setLastScroll] = useState(0);
+
+  useEffect(() => {
+    const scrollHandler = () => {
+      if (!header.current) return;
+
+      // if user is on a large screen, don't do anything
+      if (window.innerWidth > 640) return;
+
+      const currentScroll = window.scrollY;
+
+      if (currentScroll <= 60) {
+        header.current.style.transform = "";
+        return;
+      }
+
+      if (currentScroll > lastScroll && !header.current.style.transform) {
+        header.current.style.transform = "translate3d(0, -100%, 0)";
+      }
+
+      if (currentScroll < lastScroll && header.current.style.transform !== "") {
+        header.current.style.transform = "";
+      }
+
+      setLastScroll(currentScroll);
+    };
+
+    window.addEventListener("scroll", scrollHandler);
+
+    return () => {
+      window.removeEventListener("scroll", scrollHandler);
+    };
+  }, [lastScroll]);
+
   return (
-    <div className={clsx('sticky top-0 w-full border-b border-b-grayBorder bg-black/60 backdrop-blur-md z-10', props.className)}>
+    <div
+      className={clsx(
+        "sticky top-0 w-full border-b border-b-grayBorder bg-black/60 backdrop-blur-md z-10 transition-all duration-300",
+        props.className
+      )}
+      ref={header}
+    >
       {children}
     </div>
   );
--- apps/darcy/src/components/Navbar/MobileNavbar.tsx
+++ apps/darcy/src/components/Navbar/MobileNavbar.tsx
@@ -1,8 +1,44 @@
-import { NavbarLinks } from './NavbarLinks';
+"use client";
+
+import { useEffect, useRef, useState } from "react";
+import { NavbarLinks } from "./NavbarLinks";
 
 export default function MobileNavbar() {
+  const navbar = useRef<HTMLDivElement>(null);
+  const [lastScroll, setLastScroll] = useState(0);
+
+  useEffect(() => {
+    const scrollHandler = () => {
+      if (!navbar.current) return;
+
+      // if user is on a large screen, don't do anything
+      if (window.innerWidth > 640) return;
+
+      const currentScroll = window.scrollY;
+
+      if (currentScroll > lastScroll && !navbar.current.style.transform) {
+        navbar.current.style.transform = "translate3d(0, 100%, 0)";
+      }
+
+      if (currentScroll < lastScroll && navbar.current.style.transform !== "") {
+        navbar.current.style.transform = "";
+      }
+
+      setLastScroll(currentScroll);
+    };
+
+    window.addEventListener("scroll", scrollHandler);
+
+    return () => {
+      window.removeEventListener("scroll", scrollHandler);
+    };
+  }, [lastScroll]);
+
   return (
-    <div className="sticky bottom-0 w-full border-t border-t-grayBorder bg-black/80 p-1 backdrop-blur-md sm:hidden">
+    <div
+      className="sticky bottom-0 w-full border-t border-t-grayBorder bg-black/80 p-1 backdrop-blur-md sm:hidden transition-all"
+      ref={navbar}
+    >
       <nav className="flex items-center justify-evenly xl:items-start">
         <NavbarLinks.Mobile />
       </nav>
