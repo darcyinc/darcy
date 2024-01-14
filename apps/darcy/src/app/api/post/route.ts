b5b0a4d2c40053750c494f70877f4573fd593cae
Davi Patricio <davipatricio2006@gmail.com>
2024-01-14 20:05:20 -0300

    improve status code

--- apps/darcy/src/app/api/post/route.ts
+++ apps/darcy/src/app/api/post/route.ts
@@ -33,5 +33,7 @@ export async function POST(request: NextRequest) {
     }
   });
 
-  return new Response(JSON.stringify(post));
+  return new Response(JSON.stringify(post), {
+    status: 201
+  })
 }
