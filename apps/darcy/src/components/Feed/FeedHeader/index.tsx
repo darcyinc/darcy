ef127dfbca4cafec64d276a3cb7ce16323491369
Davi Patricio <davipatricio2006@gmail.com>
2024-01-14 20:17:49 -0300

    fix lightmode colors

--- apps/darcy/src/components/Feed/FeedHeader/index.tsx
+++ apps/darcy/src/components/Feed/FeedHeader/index.tsx
@@ -42,7 +42,7 @@ export default function FeedHeader({ children, ...props }: ComponentProps<'div'>
   return (
     <div
       className={clsx(
-        'sticky top-0 w-full border-b border-b-grayBorder bg-black/60 backdrop-blur-md z-10 transition-all duration-300',
+        'sticky top-0 w-full border-b border-b-grayBorder bg-white/60 dark:bg-black/60 backdrop-blur-md z-10 transition-all duration-300',
         props.className
       )}
       ref={header}
