4b76ad9f591225c70a540a70099aab94d5048f4d
Davi Patricio <davipatricio2006@gmail.com>
2024-01-14 15:12:33 -0300

    feat: add `/popular-posts` route

---
+++ apps/darcy/src/app/api/popular-posts/route.ts
@@ -0,0 +1,57 @@
+import { prisma } from '@/utils/api/prisma';
+import { NextRequest } from 'next/server';
+
+export async function GET(request: NextRequest) {
+  const searchParams = request.nextUrl.searchParams;
+  const page = Number(searchParams.get('page') ?? 1);
+  const limit = Number(searchParams.get('limit') ?? 50);
+
+  if (Number.isNaN(page) || Number.isNaN(limit)) {
+    return new Response(
+      JSON.stringify({
+        error: 'Invalid query parameters'
+      }),
+      {
+        status: 400
+      }
+    );
+  }
+
+  if (page < 1) {
+    return new Response(
+      JSON.stringify({
+        error: 'Invalid page number'
+      }),
+      {
+        status: 400
+      }
+    );
+  }
+
+  if (limit < 1 || limit > 50) {
+    return new Response(
+      JSON.stringify({
+        error: 'Invalid limit. Must be between 1 and 50'
+      }),
+      {
+        status: 400
+      }
+    );
+  }
+
+  const popularPosts = await prisma.post.findMany({
+    where: {
+      parentId: null,
+      author: {
+        private: false
+      }
+    },
+    orderBy: {
+      likeCount: 'desc'
+    },
+    take: limit,
+    skip: (page - 1) * limit
+  });
+
+  return new Response(JSON.stringify(popularPosts));
+}
