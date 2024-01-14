11812d5781e98fc5d7c417688ee3e1e41e56d55c
Davi Patricio <davipatricio2006@gmail.com>
2024-01-14 16:01:42 -0300

    fix mobile header not showing with unauthenticated users

--- apps/darcy/src/components/Navbar/NavbarProfile/MobileNavbarProfile.tsx
+++ apps/darcy/src/components/Navbar/NavbarProfile/MobileNavbarProfile.tsx
@@ -9,18 +9,16 @@ export default function MobileNavbarProfile() {
   const currentUser = useCurrentUser();
 
   return (
-    currentUser.token && (
-      <Link className="flex items-center justify-between sm:hidden w-full py-2 px-4" href={`/${currentUser.handle}`}>
-        <img alt="Your profile avatar." className="h-8 w-8 rounded-full" src="https://picsum.photos/48.webp" />
+    <div className="flex items-center justify-between sm:hidden w-full py-2 px-4">
+      {currentUser.token && <img alt="Your profile avatar." className="h-8 w-8 rounded-full" src="https://picsum.photos/48.webp" />}
 
-        <Link className="rounded-full p-2 text-textPrimary hover:bg-hoverEffect" href="/">
-          <AiOutlineAlignLeft className="text-2xl" />
-        </Link>
-
-        <button type="button" className="hover:bg-hoverEffect p-2 mx-2 rounded-full block sm:hidden">
-          <MdOutlineSettings className="text-xl" />
-        </button>
+      <Link className="rounded-full p-2 text-textPrimary hover:bg-hoverEffect" href="/">
+        <AiOutlineAlignLeft className="text-2xl" />
       </Link>
-    )
+
+      <button type="button" className="hover:bg-hoverEffect p-2 mx-2 rounded-full block sm:hidden">
+        <MdOutlineSettings className="text-xl" />
+      </button>
+    </div>
   );
 }
