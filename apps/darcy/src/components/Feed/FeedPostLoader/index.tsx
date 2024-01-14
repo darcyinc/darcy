22c0de574d0dfb107e654ea88bf0d4695ff0e188
Davi Patricio <davipatricio2006@gmail.com>
2024-01-14 19:24:33 -0300

    fix infinite loop

--- apps/darcy/src/components/Feed/FeedPostLoader/index.tsx
+++ apps/darcy/src/components/Feed/FeedPostLoader/index.tsx
@@ -9,26 +9,19 @@ interface FeedPostLoaderProps {
 export default function FeedPostLoader({ onVisible }: FeedPostLoaderProps) {
   const ref = useRef<HTMLParagraphElement>(null);
 
+  // biome-ignore lint/correctness/useExhaustiveDependencies: prevent infinite loop
   useEffect(() => {
     if (!ref.current) return;
 
     const intersectionObserver = new IntersectionObserver((entries) => {
       const entry = entries[0];
-      if (entry.isIntersecting) {
-        onVisible?.();
-      }
+      if (entry.isIntersecting) onVisible?.();
     });
 
     intersectionObserver.observe(ref.current);
 
-    return () => {
-      intersectionObserver.disconnect();
-    };
-  }, [onVisible]);
+    return () => intersectionObserver.disconnect();
+  }, []);
 
-  return (
-    <p className="my-2 text-center" ref={ref}>
-      Carregando publicações...
-    </p>
-  );
+  return <p className="my-2 text-center" ref={ref} />;
 }
