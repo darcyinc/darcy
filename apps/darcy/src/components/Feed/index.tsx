d2096563db710c2a635b4e2d8ef0c1612e1c289e
Davi Patricio <davipatricio2006@gmail.com>
2024-01-14 01:21:52 -0300

    fix mobile navbar on empty pages

--- apps/darcy/src/components/Feed/index.tsx
+++ apps/darcy/src/components/Feed/index.tsx
@@ -4,9 +4,9 @@ import MobileNavbar from '../Navbar/MobileNavbar';
 
 export default function Feed({ children }: PropsWithChildren) {
   return (
-    <div className="relative h-full border-grayBorder sm:border-x">
-      {children}
+    <>
+      <div className="relative h-full border-grayBorder sm:border-x">{children}</div>
       <MobileNavbar />
-    </div>
+    </>
   );
 }
