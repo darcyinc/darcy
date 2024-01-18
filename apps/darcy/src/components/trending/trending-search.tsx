57498e509714d6db139bb12777393afcc65d5417
Davi Patricio <davipatricio2006@gmail.com>
2024-01-18 20:45:58 -0300

    refactor(TrendingSearch): use shadcn/ui `Input` component

--- apps/darcy/src/components/trending/trending-search.tsx
+++ apps/darcy/src/components/trending/trending-search.tsx
@@ -5,6 +5,8 @@ import { useTranslations } from 'next-intl';
 import { useRouter } from 'next/navigation';
 import { useRef } from 'react';
 import { HiOutlineX, HiSearch } from 'react-icons/hi';
+import { Button } from '../ui/button';
+import { Input } from '../ui/input';
 
 export default function TrendingSearch() {
   const t = useTranslations('Trending');
@@ -22,36 +24,35 @@ export default function TrendingSearch() {
     <form
       className={clsx(
         'group',
-        'flex items-center gap-2 rounded-full p-3',
-        'border border-solid border-transparent bg-darkGray',
-        'focus-within:border-blue focus-within:bg-transparent',
-        'valid:border-blue valid:bg-transparent valid:transition-colors valid:duration-1000 valid:ease-in-out'
+        'flex items-center gap-2 rounded-full p-1 px-2',
+        'border border-solid border-transparent bg-card',
+        'focus-within:border-primary focus-within:bg-transparent',
+        'valid:border-primary valid:bg-transparent valid:transition-colors valid:duration-1000 valid:ease-in-out'
       )}
       onSubmit={handleSubmit}
     >
       <HiSearch className="ml-2 flex-shrink-0 text-xl text-muted-foreground" />
 
-      <input
+      <Input
         required
-        autoComplete="off"
-        className="w-full bg-transparent text-sm outline-none placeholder:text-muted-foreground"
-        maxLength={255}
+        name="searchapp"
         minLength={1}
+        maxLength={255}
+        className="!ring-transparent !border-transparent bg-transparent"
         placeholder={t('searchPlaceholder')}
-        ref={searchRef}
-        type="text"
       />
 
-      <button
+      <Button
         className={clsx(
           'flex h-5 w-5 items-center justify-center rounded-full bg-primary p-0.5',
           'opacity-100 transition-opacity duration-200 ease-in-out',
           'hover:opacity-80 group-invalid:hidden'
         )}
+        aria-label={'Clear search'}
         type="reset"
       >
         <HiOutlineX className="text-lg" />
-      </button>
+      </Button>
     </form>
   );
 }
