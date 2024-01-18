cbe52ae27834acb5548cc90380e82e98d33b6079
Davi Patricio <davipatricio2006@gmail.com>
2024-01-18 14:01:34 -0300

    replace `react-tooltip` with shadcn

--- apps/darcy/src/components/user-profile/badge.tsx
+++ apps/darcy/src/components/user-profile/badge.tsx
@@ -1,37 +1,30 @@
-'use client';
-
-import { useId } from 'react';
-import { MdVerified } from 'react-icons/md';
-import { Tooltip } from 'react-tooltip';
-import 'react-tooltip/dist/react-tooltip.css';
-
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '@/components/ui/tooltip';
 import { $Enums } from '@prisma/client';
 import clsx from 'clsx';
+import { MdVerified } from 'react-icons/md';
 
 interface UserBadgeProps {
   badge: $Enums.VerifiedType;
 }
 
 export default function UserBadge({ badge }: UserBadgeProps) {
-  const id = useId();
-
   return (
-    <>
-      <Tooltip id={id} className="z-20" />
-      {badge && (
-        <MdVerified
-          className={clsx(
-            'flex-shrink-0',
-            badge === 'ORGANIZATION' && 'text-yellow-400',
-            badge === 'PERSON' && 'text-primary',
-            badge === 'GOVERNMENT' && 'text-gray-400'
-          )}
-          data-tooltip-content="Verificado"
-          data-tooltip-id={id}
-          data-tooltip-place="top"
-          data-tooltip-variant={'dark'}
-        />
-      )}
-    </>
+    <TooltipProvider>
+      <Tooltip>
+        <TooltipTrigger>
+          <MdVerified
+            className={clsx(
+              'flex-shrink-0',
+              badge === 'ORGANIZATION' && 'text-yellow-400',
+              badge === 'PERSON' && 'text-primary',
+              badge === 'GOVERNMENT' && 'text-gray-400'
+            )}
+          />
+        </TooltipTrigger>
+        <TooltipContent className="bg-background">
+          <p>Verified</p>
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
   );
 }
