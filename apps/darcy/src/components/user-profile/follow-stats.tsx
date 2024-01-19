f3a847ef796cc464e84b6606bc6f41222a208fae
Davi Patricio <davipatricio2006@gmail.com>
2024-01-19 06:23:22 -0300

    fix auto scroll warning

--- apps/darcy/src/components/user-profile/follow-stats.tsx
+++ apps/darcy/src/components/user-profile/follow-stats.tsx
@@ -15,12 +15,12 @@ export default function UserFollowStats({ following, followers }: UserFollowStat
 
   return (
     <div className="flex gap-2 text-sm">
-      <Link className="hover:underline" href={`${pathname}/following`}>
+      <Link className="hover:underline" href={`${pathname}/following`} scroll={false}>
         <span className="font-bold">{following}</span>
         <span className="text-muted-foreground"> {t('following')}</span>
       </Link>
 
-      <Link className="hover:underline" href={`${pathname}/followers`}>
+      <Link className="hover:underline" href={`${pathname}/followers`} scroll={false}>
         <span className="font-bold">{followers}</span>
         <span className="text-muted-foreground"> {t('followers')}</span>
       </Link>
