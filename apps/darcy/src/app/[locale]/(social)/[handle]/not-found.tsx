2c693803bfd0c090e6aeadad3b9f14d15793d4a8
Davi Patricio <davipatricio2006@gmail.com>
2024-01-26 12:57:51 -0300

    lint

--- apps/darcy/src/app/[locale]/(social)/[handle]/not-found.tsx
+++ apps/darcy/src/app/[locale]/(social)/[handle]/not-found.tsx
@@ -5,7 +5,9 @@ export default function NotFound() {
     <div className="flex flex-col items-center justify-center h-full">
       <p className="text-4xl font-bold">404</p>
       <h1>User not found.</h1>
-      <Link href="/" className="text-primary hover:text-muted-foreground mt-2">Return to homepage?</Link>
+      <Link href="/" className="text-primary hover:text-muted-foreground mt-2">
+        Return to homepage?
+      </Link>
     </div>
   );
 }
