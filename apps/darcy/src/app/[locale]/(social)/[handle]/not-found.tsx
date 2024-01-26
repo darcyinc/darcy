eb4a3b38c45fa50804c3f034812923e09b73ec92
Davi Patricio <davipatricio2006@gmail.com>
2024-01-26 03:03:52 -0300

    improve notfound and private profiles ui

---
+++ apps/darcy/src/app/[locale]/(social)/[handle]/not-found.tsx
@@ -0,0 +1,11 @@
+import Link from 'next/link';
+
+export default function NotFound() {
+  return (
+    <div className="flex flex-col items-center justify-center h-full">
+      <p className="text-4xl font-bold">404</p>
+      <h1>User not found.</h1>
+      <Link href="/" className="text-primary hover:text-muted-foreground mt-2">Return to homepage?</Link>
+    </div>
+  );
+}
--- apps/darcy/src/app/[locale]/(social)/[handle]/page.tsx
+++ apps/darcy/src/app/[locale]/(social)/[handle]/page.tsx
@@ -5,6 +5,8 @@ import { FeedHeader } from '@/components/feed';
 import UserProfilePage from '@/features/pages/user-profile';
 import { ArrowLeft } from 'lucide-react';
 import Link from 'next/link';
+import { redirect } from 'next/navigation';
+import { notFound } from 'next/navigation';
 
 interface HomeProps {
   params: {
@@ -16,18 +18,37 @@ export default async function Home({ params }: HomeProps) {
   const handle = decodeURIComponent(params.handle);
 
   if (handle.startsWith('@')) {
-    // router.replace(`/${handle.replace('@', '')}`);
-    return null;
+    redirect(`/${handle.replace('@', '')}`);
   }
 
   const user = await apiClient.get<GetUserResponse>(`/users/${handle}`);
 
   if (user.status !== 200) {
-    return <h1>not found</h1>;
+    notFound();
   }
 
   if (user.data.private) {
-    return <h1>private profile</h1>;
+    return (
+      <>
+        <FeedHeader className="flex items-center gap-4 p-2 backdrop-blur-md">
+          <Link className="rounded-full hover:bg-accent p-2" href="/">
+            <ArrowLeft size={20} />
+          </Link>
+
+          <div>
+            <h1 className="text-lg font-bold">{user.data.displayName}</h1>
+            <p className="text-sm text-muted-foreground">{user.data.postCount} posts</p>
+          </div>
+        </FeedHeader>
+
+        <UserProfilePage data={{ ...user.data }} initialPosts={[]} />
+
+        <div className="text-center my-4">
+          <h1 className="font-bold">This user has a private profile.</h1>
+          <p>You must follow them to view their posts.</p>
+        </div>
+      </>
+    );
   }
 
   const posts = await apiClient.get<GetUserPostsResponse>(`/users/${handle}/posts`);
