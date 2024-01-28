0165675650f15b2c0036451a69eabc8428ec30bb
Davi Patricio <davipatricio2006@gmail.com>
2024-01-28 06:54:27 -0300

    lint

--- apps/darcy/src/api/useAuth.ts
+++ apps/darcy/src/api/useAuth.ts
@@ -10,7 +10,7 @@ export default function useCreateAuth() {
   const mutation = useMutation({
     mutationFn: async ({ service, code }: CreateAuthData) => {
       const request = await apiClient.post(`/auth/${service}/callback`, { code });
-      if (request.data.error) throw new Error(request.data.error)
+      if (request.data.error) throw new Error(request.data.error);
       return request.data;
     }
   });
