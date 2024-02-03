dfc573fdd7c670a352b54be602a3a4b6ecf96513
Davi Patricio <davipatricio2006@gmail.com>
2024-02-03 07:22:54 -0300

    refactor: cleanup ValidAuthProvider

--- apps/darcy/src/features/providers/valid-auth.tsx
+++ apps/darcy/src/features/providers/valid-auth.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { apiClient } from '@/api/client';
+import useUser from '@/api/useUser';
 import { useCurrentUser } from '@/hooks/useCurrentUser';
 import { usePathname, useRouter } from 'next/navigation';
 import { PropsWithChildren, useEffect } from 'react';
@@ -9,6 +9,7 @@ export default function ValidAuthProvider({ children }: PropsWithChildren) {
   const router = useRouter();
   const pathname = usePathname();
   const currentUser = useCurrentUser();
+  const { data, error } = useUser('@me');
 
   // biome-ignore lint/correctness/useExhaustiveDependencies: <explanation>
   useEffect(() => {
@@ -17,18 +18,14 @@ export default function ValidAuthProvider({ children }: PropsWithChildren) {
     // If the user has a saved token, try to GET /api/users/@me to check if it's valid
     // If it is invalid, redirect to /auth
     if (token && !pathname.includes('/auth')) {
-      apiClient.get('/users/@me').then((response) => {
-        if (response.status !== 200) {
-          localStorage.removeItem('token');
-          currentUser.setData({ token: null });
-          router.push('/auth');
-          return;
-        }
-
-        currentUser.setData({ ...response.data, token });
-      });
+      if (data) currentUser.setData({ ...data, token });
+      if (error) {
+        localStorage.removeItem('token');
+        currentUser.setData({ token: null });
+        router.push('/auth');
+      }
     }
-  }, []);
+  }, [data, error]);
 
   return children;
 }
