7de0767e4529dace9d72fde05c0e3afc67f56ef1
Davi Patricio <davipatricio2006@gmail.com>
2024-02-04 12:46:37 -0300

    feat: add react query devtools

--- apps/darcy/src/features/providers/react-query.tsx
+++ apps/darcy/src/features/providers/react-query.tsx
@@ -2,9 +2,15 @@
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { PropsWithChildren } from 'react';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 const queryClient = new QueryClient();
 
 export default function ReactQueryProvider({ children }: PropsWithChildren) {
-  return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
+      {children}
+    </QueryClientProvider>
+  );
 }
