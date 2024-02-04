d09cb344ae39a7841add5328b76163cea54e7fb1
Davi Patricio <davipatricio2006@gmail.com>
2024-02-04 12:53:49 -0300

    feat: add vercel analytics

---
+++ apps/darcy/src/features/providers/analytics.tsx
@@ -0,0 +1,14 @@
+'use client';
+
+import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
+import { PropsWithChildren } from 'react';
+export default function AnalyticsProvider({ children }: PropsWithChildren) {
+  return (
+    <>
+      <SpeedInsights />
+      <Analytics />
+      {children}
+    </>
+  );
+}
