85e84e6c16d0a15e88654ac02cf36a5e0e41c122
Davi Patricio <davipatricio2006@gmail.com>
2024-02-04 12:46:40 -0300

    lint

--- apps/darcy/src/features/providers/react-query.tsx
+++ apps/darcy/src/features/providers/react-query.tsx
@@ -1,8 +1,8 @@
 'use client';
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { PropsWithChildren } from 'react';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { PropsWithChildren } from 'react';
 
 const queryClient = new QueryClient();
 
