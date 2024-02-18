6d1dd9c622732de25b8c231882bb882d7d166735
Davi Patricio <davipatricio2006@gmail.com>
2024-02-18 01:38:28 -0300

    fix: global error page not rendering

--- apps/darcy/src/app/global-error.tsx
+++ apps/darcy/src/app/global-error.tsx
@@ -5,19 +5,16 @@ import NextError from 'next/error';
 import { useEffect } from 'react';
 
 interface LayoutProps {
-  params: {
-    locale: string;
-  };
   error: Error & { digest?: string };
 }
 
-export default function GlobalError({ error, params }: LayoutProps) {
+export default function GlobalError({ error }: LayoutProps) {
   useEffect(() => {
     Sentry.captureException(error);
   }, [error]);
 
   return (
-    <html lang={params.locale}>
+    <html lang="en">
       <body>
         {/* This is the default Next.js error component but it doesn't allow omitting the statusCode property yet. */}
         {/* biome-ignore lint/suspicious/noExplicitAny: <explanation> */}
