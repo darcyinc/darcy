5ccdb88a895eb4cd0362c18e2a6325fecc6c1689
Davi Patricio <davipatricio2006@gmail.com>
2024-02-18 01:37:44 -0300

    fix: website requests not working on Docker container

--- apps/darcy/src/api/client.ts
+++ apps/darcy/src/api/client.ts
@@ -1,7 +1,8 @@
 import ky from 'ky';
 
 export const apiClient = ky.extend({
-  prefixUrl: process.env.NEXT_PUBLIC_API_URL,
+  // if api client is created on server and app is running on Docker, SERVER_API_URL will be defined.
+  prefixUrl: process.env.SERVER_API_URL ?? process.env.NEXT_PUBLIC_API_URL,
   credentials: 'include',
   headers: {
     'Content-Type': 'application/json'
