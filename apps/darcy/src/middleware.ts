ffc275c463291eb36231c8a7d9e0183ce530d3ab
Davi Patricio <davipatricio2006@gmail.com>
2024-02-20 11:17:42 -0300

    make english the default language

--- apps/darcy/src/middleware.ts
+++ apps/darcy/src/middleware.ts
@@ -2,7 +2,7 @@ import createMiddleware from 'next-intl/middleware';
 
 export default createMiddleware({
   locales: ['en', 'pt'],
-  defaultLocale: 'pt',
+  defaultLocale: 'en',
   localePrefix: 'as-needed'
 });
 
