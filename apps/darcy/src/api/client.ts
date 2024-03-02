3c5cfa671a57c73a46a8e42aac3b8284098fd9b7
Davi Patricio <davipatricio2006@gmail.com>
2024-03-01 21:48:07 -0300

    feat: use local storage instead of cookies

--- apps/darcy/src/api/client.ts
+++ apps/darcy/src/api/client.ts
@@ -3,8 +3,16 @@ import ky from 'ky';
 export const apiClient = ky.extend({
   // if api client is created on server and app is running on Docker, SERVER_API_URL will be defined.
   prefixUrl: process.env.SERVER_API_URL ?? process.env.NEXT_PUBLIC_API_URL,
-  credentials: 'include',
+  hooks: {
+    beforeRequest: [
+      (request) => {
+        const token = localStorage?.getItem('darcy-token') ?? '';
+        request.headers.set('Authorization', `Bearer ${token}`);
+      }
+    ]
+  },
   headers: {
+    Accept: 'application/json',
     'Content-Type': 'application/json'
   }
 });
