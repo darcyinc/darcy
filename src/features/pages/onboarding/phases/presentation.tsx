799204da2e4a4569ce084799aee72b676a86698a
Davi Patricio <davipatricio2006@gmail.com>
2024-03-17 20:27:17 -0300

    add animation

--- src/features/pages/onboarding/phases/presentation.tsx
+++ src/features/pages/onboarding/phases/presentation.tsx
@@ -12,9 +12,11 @@ export default function OnboardingPresentationPhase({ setPhase }: OnboardingPhas
       <h1 className="font-bold text-4xl">Seja bem-vindo(a) à Darcy</h1>
       <p className="text-accent-foreground text-xl">Vamos começar?</p>
 
-      <Button className="w-fit gap-4 mt-4 font-bold" onClick={nextPhase}>
+      <Button className="w-fit gap-4 mt-4 font-bold group" onClick={nextPhase}>
         Definir nome e foto de perfil
-        <ArrowRight />
+        <div className="!transition-all group-hover:translate-x-1.5">
+          <ArrowRight />
+        </div>
       </Button>
     </div>
   );
