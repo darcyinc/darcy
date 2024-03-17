2d3eb1334389246af809c712b4497dc6f3b2abdd
Davi Patricio <davipatricio2006@gmail.com>
2024-03-17 16:10:54 -0300

    fix: lint

--- src/features/pages/onboarding/phases/presentation.tsx
+++ src/features/pages/onboarding/phases/presentation.tsx
@@ -1,6 +1,6 @@
 import { Button } from '@/components/ui/button';
-import type { OnboardingPhaseProps } from '..';
 import { ArrowRight } from 'lucide-react';
+import type { OnboardingPhaseProps } from '..';
 
 export default function OnboardingPresentationPhase({ setPhase }: OnboardingPhaseProps) {
   const nextPhase = () => {
