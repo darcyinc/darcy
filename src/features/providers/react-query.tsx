5afd9c6bc5824c7863cde12121c779a02d79d7ff
Davi Patricio <davipatricio2006@gmail.com>
2024-03-30 10:45:54 -0300

    fix: make query stale time infinite

--- src/features/providers/react-query.tsx
+++ src/features/providers/react-query.tsx
@@ -4,7 +4,13 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import type { PropsWithChildren } from 'react';
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: Number.POSITIVE_INFINITY
+    }
+  }
+});
 
 export default function ReactQueryProvider({ children }: PropsWithChildren) {
   return (
