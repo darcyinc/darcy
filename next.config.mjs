1fb1c35cb849e8c5d1583d3fd3a6bc556ca45be0
Davi Patricio <davipatricio2006@gmail.com>
2024-06-07 03:46:19 -0300

    feat: cleanup

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,18 @@
 import withBundleAnalyzer from '@next/bundle-analyzer';
 import { withSentryConfig } from '@sentry/nextjs';
-import { next as withMillion } from 'million/compiler';
 import nextIntl from 'next-intl/plugin';
 
 const withNextIntl = nextIntl('./src/utils/i18n.ts');
 
 export default withSentryConfig(
-  withNextIntl(
-    withBundleAnalyzer({
-      enabled: process.env.ANALYZE === 'true',
-      reactStrictMode: false,
-      ...withMillion({}, { auto: true, rsc: true })
-    })
-  ),
+  {
+    reactStrictMode: false,
+    ...withNextIntl(
+      withBundleAnalyzer({
+        enabled: process.env.ANALYZE === 'true'
+      })
+    )
+  },
   {
     // For all available options, see:
     // https://github.com/getsentry/sentry-webpack-plugin#options
@@ -20,17 +20,12 @@ export default withSentryConfig(
     // Suppresses source map uploading logs during build
     silent: true,
     org: 'darcy-c9',
-    project: 'darcy-frontned'
-  },
-  {
+    project: 'darcy-frontend',
     // For all available options, see:
     // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
 
     // Upload a larger set of source maps for prettier stack traces (increases build time)
     widenClientFileUpload: true,
-
-    // Transpiles SDK to be compatible with IE11 (increases bundle size)
-    transpileClientSDK: true,
 
     // Routes browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers (increases server load)
     tunnelRoute: '/monitoring',
